4933f5b284b917d1f15ae3aec0e0cbbee96e027e
vuaphapthuat410 <vuaphapthuat410@gmail.com>
2020-12-23 15:56:44 +0700

    Change project structure follow Laravel MVC rule

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'vietnamese-dictionary.clgtn4sxu21h.us-east-1.rds.amazonaws.com');
-   define('DB_USERNAME', 'admin');
-   define('DB_PASSWORD', 'WebIT4552');
-   define('DB_DATABASE', 'vietdict');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
