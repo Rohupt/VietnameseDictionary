ecab8c545786002bde6d1f5049159f76ad6aa09f
datnv6712 <datnv6712@gmail.com>
2020-12-28 22:15:18 +0700

    add search page front end

--- resources/views/search.php
+++ resources/views/search.php
@@ -1,30 +1,41 @@
-
 <!DOCTYPE html>
 <html>
-<head>
+<title>search result</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="../css/home.css">
-</head>
+
+<link rel='stylesheet' type='text/css' href='../css/search.css'>
 <body>
 
-    <div class="topbar">
+<div class="topbar">
       <ul>
             <li class="signin"><a href="signin">Đăng nhập/Đăng ký</a></li>
      </ul>
     </div>
-
-    <div class="logo-bar">
-      <img src="../images/logo.png" class='logo' alt=""/>
-    </div>
-
-
+<!-- Sidebar -->
+<div class="result" style="width:70%">
         <form class="searchbar">
                 <input type="text" placeholder="Search.." name="search">
                <button type="submit"><i class="fa fa-search"></i></button>
         </form>
 </div>
 
+<!-- Page Content -->
+<div class='quiz' style="margin-left:70%">
 
+    <div class="quiz-text">
+         <h1>Hỏi nhanh</h1>
+    </div>
+
+
+
+    <div class="question">
+        <h2>Từ nào sau đây đúng?</h2>
+        <p>Dành giật</p>
+        <p>Giành giật</p>
+    </div>
+
+</div>
+      
 </body>
-</html> 
+</html>
