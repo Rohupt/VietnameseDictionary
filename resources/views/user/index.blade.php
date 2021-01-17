6af7e17eaaceb7a9a4cd41cf4fa6ce0db598ce54
datnv6712 <datnv6712@gmail.com>
2021-01-18 00:02:55 +0700

    add user infomation form

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    // Show form here
+     <div class="container"
+                <div class="row md-1">
+                    <div class="col-md-12"><label class="labels">Tên:{{ Auth::user()->name }}</label></div>
+                    <div class="col-md-12"><label class="labels">Tên đầy đủ:{{ Auth::user()->fullname }}</div>
+                    <div class="col-md-12"><label class="labels">Email:{{ Auth::user()->email }}</div>
+                    <div class="col-md-12"><label class="labels">Giới tính:{{ Auth::user()->gender }}</div>
+                    <div class="col-md-12"><label class="labels">Ngày sinh:{{ Auth::user()->date_of_birth }}</div>
+                </div>
+</div>
 @endsection
