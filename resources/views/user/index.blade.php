7ab9e25bad43c0db3110fc640ab5957163b7b71b
datnv6712 <datnv6712@gmail.com>
2021-01-18 19:21:38 +0700

    fix register input err

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-< HEAD
+
 
 <div class="container">
     <div class="row justify-content-center">
@@ -9,11 +9,11 @@
         
             <div class="card-body">
                 
-                <div class="form-group row"> <label class="col col-form-label text-md-left">Tên:{{ Auth::user()->name }}</label></div>
-                <div class="form-group row"><label class="col col-form-label text-md-left">Tên đầy đủ:{{ Auth::user()->fullname }}</label></div>
-                <div class="form-group row"><label class="col col-form-label text-md-left">Email:{{ Auth::user()->email }}</label></div>
-                <div class="form-group row"><label class="col col-form-label text-md-left">Giới tính:{{ Auth::user()->gender }}</label></div>
-                <div class="form-group row"><label class="col col-form-label text-md-left">Ngày sinh:{{ Auth::user()->date_of_birth }}</label></div>
+                <div class="form-group row"> <label class="col col-form-label text-md-left">Tên:</label><label class="col col-form-label text-md-left">{{ Auth::user()->name }}</label></div>
+                <div class="form-group row"><label class="col col-form-label text-md-left">Tên đầy đủ:</label><label class="col col-form-label text-md-left">{{ Auth::user()->fullname }}</label></div>
+                <div class="form-group row"><label class="col col-form-label text-md-left">Email:</label><label class="col col-form-label text-md-left">{{ Auth::user()->email }}</label></div>
+                <div class="form-group row"><label class="col col-form-label text-md-left">Giới tính:</label><label class="col col-form-label text-md-left">{{ Auth::user()->gender }}</label></div>
+                <div class="form-group row"><label class="col col-form-label text-md-left">Ngày sinh:</label><label class="col col-form-label text-md-left">{{ Auth::user()->date_of_birth }}</label></div>
                 <a class="btn btn-info" href="{{ route('user.edit', ['user' => Auth::user()]) }}">Chỉnh sửa thông tin</a>
             </div>
         
